f9e0b450cb9a80415649a50fe94f4726db50e5e8
AndanteDevs <orrdaniel73@gmail.com>
2019-09-04 09:32:39 +0100

    v0.1.0

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour { 
+
+    public Rigidbody rb;
+
+    public float forwardsForce;
+    public float sidewaysForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game start");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardsForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if ( Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
